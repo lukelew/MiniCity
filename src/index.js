eaa6b3fbf60d36ffb2b9a30333b20bd08e6bb755
edward yang <53891309+edwarddded@users.noreply.github.com>
2020-05-24 21:32:42 +1000

    add shadow

--- src/index.js
+++ src/index.js
@@ -6,68 +6,84 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as dat from 'dat.gui';
 
 function init() {
-    var scene = new THREE.Scene()
+    var scene = new THREE.Scene();
     var ratio = window.innerWidth/window.innerHeight
-    var camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000)
-    camera.position.set(0,0,15)
-    camera.lookAt(0,0,1)
+    var camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+    camera.position.set(0,0,15);
+    camera.lookAt(0,0,1);
 
-    var renderer = new THREE.WebGLRenderer({ antialias : true})
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    var renderer = new THREE.WebGLRenderer({ antialias : true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMapType = THREE.PCFSoftShadowMap; 
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setClearColor(0x97dbf7, 1.0);
-    document.body.appendChild(renderer.domElement)
+    document.body.appendChild(renderer.domElement);
+
 
     var gridHelper = new THREE.GridHelper(100, 100, 0x666666, 0x666666);
     scene.add(gridHelper);
-    var axes = new THREE.AxesHelper(10)
-    scene.add(axes)
+    var axes = new THREE.AxesHelper(10);
+    scene.add(axes);
 
     // lights part
     var directionLight = new THREE.DirectionalLight(0xffffff, 2)
-    directionLight.position.x = 40;
-    directionLight.position.y = 10;
-    directionLight.castShadow = true
-    scene.add(directionLight)
+    directionLight.position.x = 80;
+    directionLight.position.y = 20;
+    directionLight.castShadow = true;
+    scene.add(directionLight);
 
     var directionLighHelper = new THREE.DirectionalLightHelper(directionLight, 20);
     scene.add(directionLighHelper);
 
-    var ambientLight = new THREE.AmbientLight(new THREE.Color(1,1,1), 1)
+    var ambientLight = new THREE.AmbientLight(new THREE.Color(1,1,1), 1);
     scene.add(ambientLight);
+
+
 
     // objects part
     var objects = []
-    var floorGeometry = new THREE.PlaneGeometry(100, 100, 1,1)
-    var floorMaterial = new THREE.MeshLambertMaterial({ color: 0x6666666, side: THREE.DoubleSide } )
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial)
+    var floorGeometry = new THREE.PlaneGeometry(100, 100, 1,1);
+    var floorMaterial = new THREE.MeshLambertMaterial({ color: 0x6666666, side: THREE.DoubleSide });
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = Math.PI / 2;
-    floor.position.y = -0.1
+    floor.position.y = -0.1;
     floor.receiveShadow = true;
-    scene.add(floor)
+    scene.add(floor);
 
     var loader = new GLTFLoader();
     loader.load('./src/assets/road.glb', function (gltf) {
-        gltf.scene.position.y = 1.5;
-        gltf.scene.castShadow = true
-        scene.add(gltf.scene);
-        objects.push(gltf.scene)
-    })
-    loader.load('./src/assets/house2.glb', function (gltf) {
-        gltf.scene.castShadow = true
-        scene.add(gltf.scene);
-        objects.push(gltf.scene)
-    })
+      gltf.scene.traverse( function ( node ){
+        if (node.isMesh){
+          node.castShadow = true;
+        }
+      });
+      gltf.scene.position.y = 1.5;
+      scene.add(gltf.scene);
+      objects.push(gltf.scene);
+    });
 
-    var boxGeometry = new THREE.BoxGeometry(1,1,1)
-    var boxMaterial = new THREE.MeshLambertMaterial()
-    boxMaterial.color = new THREE.Color(0x123456)
+    loader.load('./src/assets/houses1.glb', function (gltf) {
+      gltf.scene.traverse( function ( node ){
+        if (node.isMesh){
+          node.castShadow = true;
+        }
+      });
+      gltf.scene.position.y = 1.5;
+      scene.add(gltf.scene);
+      objects.push(gltf.scene);
+      //gltf.scene.
+    });
+
+
+    var boxGeometry = new THREE.BoxGeometry(1,1,1);
+    var boxMaterial = new THREE.MeshLambertMaterial({color:0x123456});
+    //boxMaterial.color = new THREE.Color(0x123456);
     var box_mesh = new THREE.Mesh(boxGeometry, boxMaterial)
-    box_mesh.position.y = 0.5
-    box_mesh.castShadow = true
-    scene.add(box_mesh)
-    objects.push(box_mesh)
+    box_mesh.position.y = 0.5;
+    box_mesh.castShadow = true;
+    scene.add(box_mesh);
+    objects.push(box_mesh);
+
 
 
     // controls part
@@ -76,11 +92,11 @@ function init() {
     var UpdateLoop = function () {
         renderer.render(scene, camera);
         orbitControls.update();
-        requestAnimationFrame(UpdateLoop)
+        requestAnimationFrame(UpdateLoop);
     }
-    requestAnimationFrame(UpdateLoop)
+    requestAnimationFrame(UpdateLoop);
 
-    
+
     var dragControls = new DragControls(objects, camera, renderer.domElement);
     var startColor
     dragControls.addEventListener('dragstart', function (event) {
@@ -93,7 +109,6 @@ function init() {
         orbitControls.enabled = true;
         event.object.material.color.set(startColor)
     });
- 
 
     // dat.Gui
     var gui = new dat.GUI();
