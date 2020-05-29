36e6362757e694852eacaeee8396c1369c9884e2
Luke Lu <ailuqun313@gmail.com>
2020-05-29 20:38:38 +0800

    Redundant part removed

--- src/objects.js
+++ src/objects.js
@@ -12,24 +12,6 @@ floor.rotation.x = Math.PI / 2;
 floor.receiveShadow = true;
 scene.add(floor)
 
-var boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
-var boxMaterial = new THREE.MeshLambertMaterial({ color: 0x00BFFF, transparent: true });
-var box = new THREE.Mesh(boxGeometry, boxMaterial);
-box.name = 'box11'
-box.position.set(10, 0.5, 0)
-box.castShadow = true;
-box.receiveShadow = true;
-scene.add(box)
-moveableObjects.push(box)
-
-var box2 = box.clone()
-box2.name = 'box22'
-box2.position.set(20, 0.5, 0)
-box2.castShadow = true;
-box2.receiveShadow = true;
-scene.add(box2)
-moveableObjects.push(box2)
-
 loader.load('./src/assets/house.glb', function (gltf) {
     gltf.scene.traverse(function (node) {
         if (node.isMesh) {
@@ -37,7 +19,7 @@ loader.load('./src/assets/house.glb', function (gltf) {
         }
     });
     console.log(gltf);
-    gltf.scene.position.y = 1.5;
+    gltf.scene.position.y = 0;
     gltf.scene.name = 'House'
     scene.add(gltf.scene)
     moveableObjects.push(gltf.scene);
