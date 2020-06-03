88fc197e91e54f1b25a0e264f6e831c845c5ca5a
edward yang <53891309+edwarddded@users.noreply.github.com>
2020-06-03 18:43:21 +1000

    orbitcontrols

--- src/index.js
+++ src/index.js
@@ -56,13 +56,13 @@ function init() {
         renderer.render(scene, camera);
     }
     window.addEventListener( 'resize', onWindowResize, false );
+    function onWindowResize() {
+    	camera.aspect = window.innerWidth / window.innerHeight;
+    	camera.updateProjectionMatrix();
+    	renderer.setSize( window.innerWidth, window.innerHeight );
+    }
 }
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	orbitControls.handleResize();
-}
+
 
 
 function mouseDownToSelectObj(e) {
